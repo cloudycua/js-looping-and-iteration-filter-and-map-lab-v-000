e05bdb6020c0d97e83891118f0f461a253031abb
Christine Cua <cloudycua@gmail.com>
2019-02-13 15:10:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here:
+function driversWithRevenueOver(drivers, revenue) {
+  return drivers.filter(function(driver)) {
+    
+  }
+}
+
+function driversWithRevenue(drivers, revenue) {
+  const newDrivers = map(drivers, function(driver)) {
+    return Object.assign({}, driver, { revenue: 'revenue'});
+  }
+  return newDrivers;
+}
