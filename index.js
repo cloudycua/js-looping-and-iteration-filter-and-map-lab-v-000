3b9b08b181307b9b6a9af4c5a5e4d9761637bf1c
Christine Cua <cloudycua@gmail.com>
2019-02-13 16:08:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,10 +11,14 @@ function driverNamesWithRevenueOver(drivers, revenue) {
   });
 }
 
-function exactMatch(drivers, attribute) {
-  return drivers.filter(function(driver) {
-    for (const key in attribute) {
-      return driver[key] === attribute[key];
+function exactMatch (drivers, matcher) {
+  return drivers.filter(function (driver) {
+    let matches = false;
+
+    for (const key in matcher) {
+      matches = driver[key] === matcher[key];
     }
+
+    return matches;
   });
-};
+}
