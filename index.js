33961ca1c07a4b6a57deb939bff44ce94b756345
Christine Cua <cloudycua@gmail.com>
2019-02-13 15:15:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function driversWithRevenueOver(drivers, revenue) {
   return drivers.filter(function(driver)) {
-    return driver[revenue] >= revenue;
+    return driver[revenue] > revenue;
   }
 }
