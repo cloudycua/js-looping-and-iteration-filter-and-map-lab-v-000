5b0f6757871fc87ba2d42116363d2f9bcd3960f1
Christine Cua <cloudycua@gmail.com>
2019-02-13 16:09:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,12 +13,8 @@ function driverNamesWithRevenueOver(drivers, revenue) {
 
 function exactMatch (drivers, matcher) {
   return drivers.filter(function (driver) {
-    let matches = false;
-
-    for (const key in matcher) {
+    !!for (const key in matcher) {
       matches = driver[key] === matcher[key];
     }
-
-    return matches;
   });
 }
